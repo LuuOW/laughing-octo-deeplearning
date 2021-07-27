ba9f7a45fd2d6787ef518d05c94407490e6f495b
NonOfUrBusiness <lucas.kempe@icloud.com>
2021-07-26 22:06:37 -0300

    new things...

---
+++ try.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv('https://sololearn.com/uploads/files/titanic.csv')
+df['male'] = df['Sex'] == 'male'
+X = df[['Pclass', 'male', 'Age', 'Siblings/Spouses', 'Parents/Children', 'Fare']].values
+y=[0, 0, 0, 1, 1] 
+
+
+model = LogisticRegression()
+model.fit(X, y)
+
+y_pred = model.predict(X)
+print((y == y_pred).sum())
+print((y == y_pred).sum() / y.shape[0])
+print(model.score(X, y))
